846b7ab32cafdbf894ebaf2fa086bf001340fe09
guillermomartinezmonzon <guillermomartinez@gmonzon.com>
2022-01-30 18:11:48 +0100

    Updates

--- src/redux/index.js
+++
@@ -1,4 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import rootReducer from "./rootReducer";
-
-export default configureStore({ reducer: rootReducer });
--- src/utils/PrivateRoute.js
+++
@@ -1,7 +0,0 @@
-import { Navigate } from "react-router";
-import { useAuthState } from "../context";
-
-export default function PrivateRoute({ children }) {
-  const userDetails = useAuthState(); //read user details from context
-  return userDetails.token ? children : <Navigate to="/login" />;
-}
